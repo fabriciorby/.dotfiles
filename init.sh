45e0f1c67df361f35b7804523a31b3ee8a2c3514
fabriciorby <fabriciorby@hotmail.com>
2024-09-13 01:06:30 +0200

    add bat and delta config

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 brew install neovim
 brew install clojure
 brew install borkdude/brew/babashka
+brew install bat
+brew install git-delta
 
 touch ~/Library/LaunchAgents/com.local.KeyRemapping.plist
 cat remap.plist >> ~/Library/LaunchAgents/com.local.KeyRemapping.plist
