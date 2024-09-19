c97c782e35b766e087ac4f0095e49afa329946b4
fabriciorby <fabriciorby@hotmail.com>
2024-09-19 19:58:04 +0200

    configure iterm2

--- init.sh
+++ init.sh
@@ -9,7 +9,13 @@ brew install neovim
 brew install clojure
 brew install borkdude/brew/babashka
 
-
+# Remap Esc to CapsLock
 touch ~/Library/LaunchAgents/com.local.KeyRemapping.plist
 cat remap.plist >> ~/Library/LaunchAgents/com.local.KeyRemapping.plist
 launchctl load ~/Library/LaunchAgents/com.local.KeyRemapping.plist
+
+# iTerm2
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/.config/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
