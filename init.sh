48c93edf9f1da645c1628dfa8b53078fada572c3
fabriciorby <fabriciorby@hotmail.com>
2024-10-02 19:59:57 +0200

    click and hold works

--- init.sh
+++ init.sh
@@ -24,4 +24,7 @@ defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfile
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
+# remove press and hold
+defaults write -g ApplePressAndHoldEnabled -bool false
+
 bb init.clj
