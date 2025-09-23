0b0bcddac461ead9a28b6ec0cb94972c26822172
fabriciorby <fabriciorby@hotmail.com>
2025-09-23 22:38:28 +0200

    fix dotfiles in starship

--- .scripts/rds-tmux-create-session.sh
+++ .scripts/rds-tmux-create-session.sh
@@ -7,12 +7,12 @@ IFS=$'\n\t'
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$((find ~/Desktop/projects/ ~/Desktop/projects/waes -mindepth 1 -maxdepth 1 -type d -exec sh -c "echo {} | cut -d '/' -f 6-" \;; echo '.dotfiles') | fzf)
+    selected=$((find ~/Desktop/projects/ ~/Desktop/projects/waes -mindepth 1 -maxdepth 1 -type d -exec sh -c "echo {} | cut -d '/' -f 6-" \;; echo 'dotfiles') | fzf)
 fi
 
 function get_absolute_path() {
-    if [[ "$1" == ".dotfiles" ]]; then
-        echo "$HOME/$1"
+    if [[ "$1" == "dotfiles" ]]; then
+        echo "$HOME/.$1"
     else
         echo "$HOME/Desktop/projects/$selected"
     fi
@@ -33,4 +33,4 @@ if ! tmux has-session -t="$selected" 2> /dev/null; then
     tmux new-session -ds "$selected" -c "$(get_absolute_path $selected)"
 fi
 
-tmux switch-client -t $(echo "$selected" | tr . _)
+tmux switch-client -t $(echo "$selected")
