61afeef66387c0aefa180a7c403b97c2b883e8db
fabriciorby <fabriciorby@hotmail.com>
2025-10-03 02:52:14 +0200

    update to use with mason v2 - add fennel

--- .config/nvim/fnl/main.lua
+++ .config/nvim/fnl/main.lua
@@ -454,36 +454,38 @@ require("lazy").setup({
 			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-			local servers = {
-				-- clangd = {},
-				-- gopls = {},
-				-- pyright = {},
-				-- rust_analyzer = {},
-				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				--
-				-- Some languages (like typescript) have entire language plugins that can be useful:
-				--    https://github.com/pmizio/typescript-tools.nvim
-				--
-				-- But for many setups, the LSP (`ts_ls`) will work just fine
-				-- ts_ls = {},
-				--
 
-				lua_ls = {
-					-- cmd = { ... },
-					-- filetypes = { ... },
-					-- capabilities = {},
-					settings = {
-						Lua = {
-							completion = {
-								callSnippet = "Replace",
-							},
-							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-							-- diagnostics = { disable = { 'missing-fields' } },
+			vim.lsp.config('lua_ls', {
+				settings = {
+					Lua = {
+						completion = {
+							callSnippet = "Replace",
 						},
+						-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+						diagnostics = { disable = { 'missing-fields' } },
 					},
 				},
-			}
-
+			})
+			vim.lsp.config('fennel_language_server', {
+			    -- replace it with true path
+			    cmd = {'fennel-language-server'},
+			    filetypes = {'fennel'},
+			    single_file_support = true,
+			    -- source code resides in directory `fnl/`
+			    root_dir = vim.fs.root(0, { ".git", "fnl" }) or vim.fn.getcwd(),
+			    settings = {
+			      fennel = {
+				workspace = {
+				  -- If you are using hotpot.nvim or aniseed,
+				  -- make the server aware of neovim runtime files.
+				  library = vim.api.nvim_list_runtime_paths(),
+				},
+				diagnostics = {
+				  globals = {'vim'},
+				},
+			      },
+			    },
+			})
 			-- Ensure the servers and tools above are installed
 			--
 			-- To check the current status of installed tools and/or manually install
@@ -506,17 +508,8 @@ require("lazy").setup({
 			require("mason-lspconfig").setup({
 				ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
 				automatic_installation = false,
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						-- This handles overriding only values explicitly passed
-						-- by the server configuration above. Useful when disabling
-						-- certain features of an LSP (for example, turning off formatting for ts_ls)
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
-					end,
-				},
 			})
+
 		end,
 	},
 
